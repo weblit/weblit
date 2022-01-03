55dc24651601209453f12ce0ed5e229616af7499
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-03 14:29:19 +0530

    Load raw css styles using get_raw_css

--- src/get_raw_css.py
+++ src/get_raw_css.py
@@ -1,24 +1,12 @@
-import re as regex
-from src.get_raw_html import get_raw_html
+from src.get_linked_css import get_linked_css
+from src.load_response import load_response
 
-"""
-Gets all linked css associated to the specific url
-"""
-def get_linked_css(url):
-    final_matches = []
+def get_raw_css(url):
+    styles_url = get_linked_css(url)
+    bundled_styles = []
 
-    res = str(get_raw_html(url))
+    for style_url in styles_url:
+        res = load_response(style_url)
 
-    matches = regex.findall('link .*', res)
-
-    for _match in matches:
-        match = str(_match)
-
-        if match.find("href=") != -1:
-            start_index = match.index("href='")
-            end_index = -2
-
-            final_matches.append(match[start_index+6:end_index-1])
-
-    
-    return final_matches
+        print(res.content)
+        # append to bundled and output as files
