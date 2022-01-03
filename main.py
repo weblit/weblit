333f275e6bad2e503e2df3595eff9170d5954d8b
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-03 14:14:52 +0530

    add get linked css urls

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-import requests
+from src.get_raw_css import get_linked_css
+from src.get_raw_html import get_raw_html
+
+res = get_linked_css("https://hashable.space")
+
+print(res)
--- src/get_raw_html.py
+++ src/get_raw_html.py
@@ -3,6 +3,4 @@ import requests
 def get_raw_html(url):
     r = requests.get(url)
 
-    print(r)
-
-    return r.content
+    return r.content.decode()
