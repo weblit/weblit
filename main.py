bdba8bf93a8ba19f967a36989067b4e896024cfb
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-12 22:27:40 +0530

    test `github.com`

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+from src.get_linked_css import get_linked_css
 from src.get_raw_css import get_raw_css
 from src.get_raw_html import get_raw_html
 from src.get_stylesheet_count import get_stylesheet_count
 from src.load_response import load_response
 
-styles = get_raw_css("https://hashable.space")
-
-print(get_stylesheet_count("https://hashable.space"))
+print(get_linked_css("https://github.com"))
