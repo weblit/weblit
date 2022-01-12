c13bbfb05cdef3c6789593cf6e880cf7a4ad5e29
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-12 19:53:27 +0530

    Add Get Stylesheet count api

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from src.get_raw_css import get_raw_css
 from src.get_raw_html import get_raw_html
+from src.get_stylesheet_count import get_stylesheet_count
 from src.load_response import load_response
 
-get_raw_css("https://hashable.space")
+styles = get_raw_css("https://hashable.space")
+
+print(get_stylesheet_count("https://hashable.space"))
